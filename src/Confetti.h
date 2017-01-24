f765f54d0d46ebabddd4a06c42ca8cd37b115b54
Ingo Schommer <ingo@silverstripe.com>
2017-01-24 22:23:25 +1300

    Colourful drop for Confetti

--- src/Confetti.h
+++ src/Confetti.h
@@ -15,7 +15,13 @@ class Confetti: public Pattern {
     {
       fadeToBlackBy( state->leds, state->ledsSize, 10);
       int pos = random16(state->ledsSize);
-      state->leds[pos] += CHSV( gHue + random8(64), 200, 255);
+      if (isDropping) {
+        // Colorful
+        state->leds[pos] += CHSV( gHue + random8(64), 200, 255);
+      } else {
+        // Default Palette
+        state->leds[pos] += CHSV( 255, 200, 255);
+      }
 
       EVERY_N_MILLISECONDS( 20 ) { gHue++; }
     }
