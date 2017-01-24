badb803a6a986725f3c5dc1a8e98f6bcf3418842
Ingo Schommer <ingo@silverstripe.com>
2017-01-25 10:34:57 +1300

    Drop mode for sinelon, better BPM adherence

--- src/Sinelon.h
+++ src/Sinelon.h
@@ -14,14 +14,20 @@ class Sinelon: public Pattern {
     void loopForState(PatternState *state, byte fade)
     {
       fadeToBlackBy( state->leds, state->ledsSize, 20);
-      int pos = beatsin16(13, 0, state->ledsSize);
-      state->leds[pos] += CHSV( gHue, 255, 192);
+      int pos = beatsin16(bpm/8, 0, state->ledsSize);
+      if (isDropping) {
+          state->leds[pos] += CHSV( gHue, 0, 255); // white
+      } else {
+        state->leds[pos] += CHSV( gHue, 255, 192);
+      }
+
 
       EVERY_N_MILLISECONDS( 20 ) { gHue++; }
     }
 
-    int getFrameLength()
+    virtual int getFrameLength()
     {
-      return 1000 / bpm / 2;
+      return 1000 / 60; // run a bit faster to give beatsin16 enough samples
     }
+
 };
